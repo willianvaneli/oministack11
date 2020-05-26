830fe983686f5799dc62bce290a9efa494773fc6
willianvaneli <willianvaneli.gmail.com>
2020-05-25 21:13:18 -0300

    mobile2

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,81 @@
 import React from 'react';
-import {View} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import {View, Text, Image, TouchableOpacity, Linking} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
+
+
+
+import styles from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 export default function Detail(){
+    const navigation = useNavigation();
+    const route = useRoute();
+    const incident = route.params.incident;
+
+    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$ 120,00. ';
+
+    function navigateBack(){
+        navigation.goBack()
+    }
+
+    function sendMail(){
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: Cadelinha atropelada',
+            recipients: ['diego@rocketsear.com.br'],
+            body: message,
+        })
+    }
+
+    function sendWhatsApp(){
+        Linking.openURL(`whatsapp://send?phone=5527997271914&text=${message}`);
+    }
+
     return (
-        <View/>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+
+                <TouchableOpacity onPress={navigateBack}>
+                    <Feather name ="arrow-left" size={28} color="#E82041"/>
+                </TouchableOpacity>
+            </View>
+
+
+            <View style={styles.incident}>
+                <Text style={styles.incidentProperty}>ONG:</Text>
+                <Text style={styles.incidentValue}>{incident.name}</Text>
+
+                <Text style={styles.incidentProperty}>CASO:</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
+
+                <Text style={styles.incidentProperty}>VALOR:</Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR', {
+                        style:'currency',
+                        currency: 'BRL'}).format(incident.value)}</Text>
+                    
+            </View>
+
+            <View style={styles.contactBox}>
+                <Text style={styles.heroTitle}>Salve o dia!</Text>
+                <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+                <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
+                        <Text style={styles.actionText}>WhatsApp</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
+                        <Text style={styles.actionText}>E-mail</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+        </View>
     );
 }
